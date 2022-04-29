3e6c6d2791eae24b98b09ca24347101bd91b3e4e
ajithgaade <ajithreddy428@gmail.com>
2022-04-28 21:35:51 -0400

    initial git integration

---
+++ src/main/java/com/garrsolutions/yelpfusionintegration/YelpFusionIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.garrsolutions.yelpfusionintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpFusionIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YelpFusionIntegrationApplication.class, args);
+    }
+
+}
